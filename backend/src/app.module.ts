65b82126c9cfc524d74f469ed5debb09963e7933
Pappszii <erdelyidanii@gmail.com>
2025-09-16 15:56:11 +0200

    add init migration, fix dockerfile, simple user controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
+import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from 'prisma/prisma.service';
+import { UsersService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UsersService],
 })
 export class AppModule {}
